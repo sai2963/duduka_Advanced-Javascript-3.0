46bda5d6fc7a30d0162b2d6020dfadc10aa22675
sai2963 <venkatsai0629@gmail.com>
2023-12-04 21:07:43 +0530

    call backs added

---
+++ Sync_Asnync.js
@@ -0,0 +1,15 @@
+//sync
+console.log(" I");
+console.log("eat");
+console.log("Ice Cream");
+console.log("with");
+console.log("spoon");
+//async
+
+console.log(" I");
+console.log("eat");
+setTimeout(() => {
+  console.log("Ice Cream ");
+});
+console.log("with");
+console.log("spoon");
--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//with normal  function
 function one(call) {
   console.log("one");
   call();
@@ -6,3 +7,46 @@ function two() {
   console.log("two");
 }
 one(two);
+//with arrow function
+let stocks = {
+  friuts: ["strawberry", "apple", "mango", "bannana"],
+  liqiud: ["water", "juice"],
+  holder: ["cone", "cup", "stick"],
+  toopings: ["choclate ", "peanuts"],
+};
+let order = (friut_name, call_production ,served) => {
+  setTimeout(() => {
+    console.log(`${stocks.friuts[friut_name]}`);
+    call_production();
+    served();
+  }, 2000);
+};
+let production = () => {
+  setTimeout(() => {
+    console.log("production has started");
+    setTimeout(() => {
+      console.log("The friuts are chopped");
+      setTimeout(() => {
+        console.log(`added ${stocks.liqiud[0]} and ${stocks.liqiud[1]}`);
+        setTimeout(() => {
+          console.log(`The Machine has started`);
+          setTimeout(() => {
+            console.log(`The container is ${stocks.holder[1]}`);
+            setTimeout(() => {
+              console.log(`The topings selected are ${stocks.toopings[0]} and ${stocks.toopings[1]}`);
+              setTimeout(()=>{
+                console.log('The Ice crream has been Served')
+              },2000)
+            }, 3000);
+          }, 2000);
+        }, 1000);
+      }, 1000);
+    }, 2000);
+  }, 0);
+};
+let serve=()=>{
+  setTimeout(()=>{
+    console.log('The Ice crream has been Served')
+  },2000)
+}
+order(0, production,serve);
