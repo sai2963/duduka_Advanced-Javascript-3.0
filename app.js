886017c70456560fa2076aecb904ba178d3a1001
sai2963 <venkatsai0629@gmail.com>
2023-12-05 14:07:50 +0530

    promises added

--- app.js
+++ app.js
@@ -1,52 +1,68 @@
-//with normal  function
-function one(call) {
-  console.log("one");
-  call();
-}
-function two() {
-  console.log("two");
-}
-one(two);
-//with arrow function
 let stocks = {
-  friuts: ["strawberry", "apple", "mango", "bannana"],
-  liqiud: ["water", "juice"],
+  fruits: ["strawberry", "apple", "mango", "banana"],
+  liquid: ["water", "juice"],
   holder: ["cone", "cup", "stick"],
-  toopings: ["choclate ", "peanuts"],
+  toppings: ["chocolate", "peanuts"],
 };
-let order = (friut_name, call_production ,served) => {
-  setTimeout(() => {
-    console.log(`${stocks.friuts[friut_name]}`);
-    call_production();
-    served();
-  }, 2000);
-};
-let production = () => {
-  setTimeout(() => {
-    console.log("production has started");
-    setTimeout(() => {
-      console.log("The friuts are chopped");
+
+let is_shop_open = true;
+
+let order = (time, work) => {
+  return new Promise((resolve, reject) => {
+    if (is_shop_open) {
       setTimeout(() => {
-        console.log(`added ${stocks.liqiud[0]} and ${stocks.liqiud[1]}`);
-        setTimeout(() => {
-          console.log(`The Machine has started`);
-          setTimeout(() => {
-            console.log(`The container is ${stocks.holder[1]}`);
-            setTimeout(() => {
-              console.log(`The topings selected are ${stocks.toopings[0]} and ${stocks.toopings[1]}`);
-              setTimeout(()=>{
-                console.log('The Ice crream has been Served')
-              },2000)
-            }, 3000);
-          }, 2000);
-        }, 1000);
-      }, 1000);
-    }, 2000);
-  }, 0);
+        resolve(work());
+      }, time);
+    } else {
+      reject(
+        ("Sorry for the inconvenience, our shop is not open right now")
+      );
+    }
+  });
 };
-let serve=()=>{
-  setTimeout(()=>{
-    console.log('The Ice crream has been Served')
-  },2000)
-}
-order(0, production,serve);
+order(2000,()=>(console.log(`${stocks.fruits[0]}`)))
+
+.then(()=>{
+  return order(0,()=>(console.log('The Production has Started')))
+})
+
+.then(()=>{
+  return order(2000,()=>(console.log('The Friut Has been Chopped')))
+})
+
+.then(()=>{
+  return order(1000,()=>(console.log(`Added ${stocks.liquid[0]} and ${stocks.liquid[1]}`)))
+})
+
+.then(()=>{
+  return order(1000,()=>(console.log('The machine has started')))
+})
+
+.then(()=>{
+  return order(2000,()=>(console.log(`The Ice cream is on the ${stocks.holder[0]}`)))
+})
+
+.then(()=>{
+  return order(3000,()=>(console.log(`the toping is ${stocks.toppings[0]}`)))
+})
+
+.then(()=>{
+  return order(2000,()=>(console.log('The Ice Cream Has BEen Served')))
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
